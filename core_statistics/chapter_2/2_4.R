fad82956037da4141825c4f6fc1521388a315d74
paroussisc <christopher.paroussis@gmail.com>
2018-04-18 14:56:07 +0100

    Add generalised likelihood ratio test for CI in 2.4

--- core_statistics/chapter_2/2_4.R
+++ core_statistics/chapter_2/2_4.R
@@ -1,28 +1,38 @@
-############# Solutions to question 2.4 #############
+############## Solutions to question 2.4 #############
 library(ggplot2)
 
 ll <- function(theta)
 {
-  allo <- c(28,32,49, 84, 357)
+  allo <- c(28, 32, 49, 84, 357)
   allo_cens <- c(933, 1078, 1183, 1560, 2114, 2144)
   
   allo_lik = sum(dexp(allo, theta, log = TRUE))
-  allo_cens_lik = sum(-theta*allo_cens)
+  allo_cens_lik = sum(-theta * allo_cens)
   return (allo_lik + allo_cens_lik)
 }
 
 # ggplot contour
-thetas <- seq(0.00001,0.001, 0.00001)
-df.lik <- setNames(expand.grid(thetas), c('theta')) 
+thetas <- seq(0.00001, 0.001, 0.00001)
+df.lik <- setNames(expand.grid(thetas), c('theta'))
 vfun1 <- Vectorize(ll, SIMPLIFY = TRUE)
 
-df.lik$z <- vfun1(df.lik$theta)       
-p <- ggplot(df.lik, aes(theta, z=z)) + geom_point(aes(x=theta, y = z))
+df.lik$z <- vfun1(df.lik$theta)
+p <-
+  ggplot(df.lik, aes(theta, z = z)) + geom_point(aes(x = theta, y = z))
 p
 
 # max
-mll <- df.lik[52,]$theta
+mll <- df.lik[52, ]$theta
+mll_z <- df.lik[52, ]$z
 
-# CI
-mll*(1 + c(-1,1)*1.96/sqrt(11))
+llf <- function(theta)
+{
+  return(2*(mll_z - ll(theta)) - qchisq(0.95,1))
+}
 
+# CI - generalised likelihood ratio test
+uniroot(llf,c(.000000001,mll))$root # lower
+uniroot(llf,c(mll,1))$root # upper
+
+# CI  - normal approx
+mll * (1 + c(-1, 1) * 1.96 / sqrt(11))

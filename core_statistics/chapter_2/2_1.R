60724d2e5dc44247c2ca8c427191c427047836eb
paroussisc <christopher.paroussis@gmail.com>
2018-05-03 08:06:06 +0100

    Tidy up 2.1

--- core_statistics/chapter_2/2_1.R
+++ core_statistics/chapter_2/2_1.R
@@ -5,16 +5,10 @@ sig <- sd(nhtemp)
 ci_mu <- qnorm(c(0.025, 0.975), mu, sig)
 ci_alt <- mu + sig * qnorm(c(0.025, 0.975))
 
-ci_sd <- c(sqrt(((length(
-  nhtemp
-) - 1) * sd(nhtemp) ^ 2) / qchisq(
-  c(.025), df = length(nhtemp) - 1, lower.tail = FALSE
-)),
-sqrt(((length(
-  nhtemp
-) - 1) * sd(nhtemp) ^ 2) / qchisq(
-  c(.975), df = length(nhtemp) - 1, lower.tail = FALSE
-)))
+#
+df <- length(nhtemp) - 1
+ci_sd <-
+  sig *  sqrt(df / qchisq(c(.025, .975), df = df, lower.tail = FALSE))
 
 print("CI on mu:")
 print(ci_mu)

8f66c79f41fb563c509583a006d02a8bcac597eb
paroussisc <38275805+paroussisc@users.noreply.github.com>
2018-05-04 17:01:28 +0100

    Update 2_3.R
    Make this better using comment.

--- core_statistics/chapter_2/2_3.R
+++ core_statistics/chapter_2/2_3.R
@@ -1,19 +1,22 @@
 ############# Solutions to question 2.3 #############
 library(ggplot2)
 
-ll <- function(mu, sigma)
+ll <- function(mu, sigma, y)
 {
-  alpha = 3
-  return (sum(log((1/sigma) * dt((nhtemp - mu)/sigma, alpha))))
+  alpha <- 3
+  n <- length(y)
+  return (n * log(1/sigma) + sum(dt((y - mu)/sigma, alpha, log = TRUE)))
 }
 
 # ggplot contour
 mus <- seq(30,70, 0.1)
 sigs <- seq(0.1,15,0.1)
+
+# apply(expand.grid(mus, sigs), 1, function(x){ll(x[1], x[2], y = nhtemp)}) - try using apply
 df.lik <- setNames(expand.grid(mus, sigs), c('mu', 'sigma')) 
 vfun1 <- Vectorize(ll, SIMPLIFY = TRUE)
 
-df.lik$z <- vfun1(df.lik$mu,df.lik$sigma)       
+df.lik$z <- vfun1(df.lik$mu,df.lik$sigma, nhtemp)       
 ggplot(df.lik, aes(mu, sigma, z=z)) + stat_contour(aes(colour = ..level..))
 
 

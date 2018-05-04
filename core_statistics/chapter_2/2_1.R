a83d65348ab9a30b79a424df716b0ffaa288618b
paroussisc <38275805+paroussisc@users.noreply.github.com>
2018-05-04 16:27:27 +0100

    Update 2_1.R
    Fix typo

--- core_statistics/chapter_2/2_1.R
+++ core_statistics/chapter_2/2_1.R
@@ -1,12 +1,13 @@
 ############# Solutions to question 2.1 #############
-
+n <- length(nhtemp)
 mu <- mean(nhtemp)
 sig <- sd(nhtemp)
-ci_mu <- qnorm(c(0.025, 0.975), mu, sig)
-ci_alt <- mu + sig * qnorm(c(0.025, 0.975))
+st_err <- sig/sqrt(n)
+ci_mu <- qnorm(c(0.025, 0.975), mu, st_err)
+ci_alt <- mu + st_err * qnorm(c(0.025, 0.975))
 
 #
-df <- length(nhtemp) - 1
+df <- n - 1
 ci_sd <-
   sig *  sqrt(df / qchisq(c(.025, .975), df = df, lower.tail = FALSE))
 

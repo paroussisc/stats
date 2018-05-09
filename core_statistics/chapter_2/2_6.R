550998439e015d078bed5ef7631acf5a221f5ad2
paroussisc <christopher.paroussis@gmail.com>
2018-05-09 11:17:09 +0100

    Format.

--- core_statistics/chapter_2/2_6.R
+++ core_statistics/chapter_2/2_6.R
@@ -4,17 +4,17 @@ x <- rnorm(10) + 1
 # integrate params out of prior * likelihood to get marginal distribution of the model
 bayes_2 <- function(theta)
 {
-  return(dunif(theta, -2, 2, log = TRUE) + sum(dnorm(x, theta, 1, log = TRUE)))
+  return(dunif(theta,-2, 2, log = TRUE) + sum(dnorm(x, theta, 1, log = TRUE)))
 }
 
 bayes_20 <- function(theta)
 {
-  return(dunif(theta, -20, 20, log = TRUE) + sum(dnorm(x, theta, 1, log = TRUE)))
+  return(dunif(theta,-20, 20, log = TRUE) + sum(dnorm(x, theta, 1, log = TRUE)))
 }
 
 # Integrate over params and evaluate Bayes' factor
-M0 <- integrate(bayes_2,-2, 2)
-M1 <- integrate(bayes_20,-20, 20)
+M0 <- integrate(bayes_2, -2, 2)
+M1 <- integrate(bayes_20, -20, 20)
 
 # value of >10 is very strong evidence against M0
 print(2 * log(M1$value / M0$value))

d6ef2d25ad9bc14dbf4b428bfbd4b98b679356e5
paroussisc <christopher.paroussis@gmail.com>
2018-05-15 18:19:20 +0100

    Updates.

--- core_statistics/chapter_2/2_4.R
+++ core_statistics/chapter_2/2_4.R
@@ -8,16 +8,17 @@ ll <- function(theta, data, data_cens)
   return (lik + cens_lik)
 }
 
+
+llf <- function(theta, data, data_cens, mll_z, alpha)
+{
+  return(2 * (mll_z - ll(theta, data, data_cens)) - qchisq(1 - alpha, 1))
+}
+
 allo <- c(28, 32, 49, 84, 357)
 allo_cens <- c(933, 1078, 1183, 1560, 2114, 2144)
 
-llf <- function(theta, data, data_cens)
-{
-  return(2 * (mll_z - ll(theta, data, data_cens)) - qchisq(0.95, 1))
-}
-
 # ggplot contour
-thetas <- seq(0.00001, 0.001, 0.00001)
+thetas <- seq(0.00001, 0.001, 0.00000001)
 df.lik <- setNames(expand.grid(thetas), c('theta'))
 df.lik$z  <-
   apply(df.lik, 1, function(x) {
@@ -27,12 +28,33 @@ ggplot(df.lik, aes(theta, z = z)) + geom_point(aes(x = theta, y = z))
 
 # max param and likelihood values
 idx <-  which.max(df.lik$z)
-mll <- df.lik[idx,]$theta
-mll_z <- df.lik[idx,]$z
+mll <- df.lik[idx, ]$theta
+mll_z <- df.lik[idx, ]$z
 
 # CI - generalised likelihood ratio test
-l <- uniroot(llf, c(.000000001, mll), data = allo, data_cens = allo_cens)$root # lower
-u <- uniroot(llf, c(mll, 1), data = allo, data_cens = allo_cens)$root # upper
+alpha <- 0.05
+l <-
+  uniroot(
+    llf,
+    c(.00000000, mll),
+    data = allo,
+    data_cens = allo_cens,
+    mll_z = mll_z,
+    alpha = alpha,
+    check.conv = TRUE,
+    tol = .Machine$double.eps
+  )$root # lower
+u <-
+  uniroot(
+    llf,
+    c(mll, 1),
+    data = allo,
+    data_cens = allo_cens,
+    mll_z = mll_z,
+    alpha = alpha,
+    check.conv = TRUE,
+    tol = .Machine$double.eps
+  )$root # upper
 
 # interval on days
-print(c(1/u, 1/l))
+print(c(1 / u, 1 / l))
